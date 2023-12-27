fc6069bec031881deb44d413684b33fd2fed2caa
aly0na27 <alenakravchenko1910@gmail.com>
2023-12-27 03:00:44 +0300

    type text

--- src/Components/Text/Text.tsx
+++ src/Components/Text/Text.tsx
@@ -1,33 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import styles from "./Text.module.css"
 
 const Text: React.FC = () => {
     const text = ["server", "hello", "cat", "thing", "will", "never", "ever", "almost"]
-    // const [position, setPosition] = useState()
+    const [typedText, setTypedText] = useState("")
+
+    const onChangeInput = (e: React.FormEvent<HTMLInputElement>) => {
+        setTypedText(e.currentTarget.value)
+        debugger
+    }
+
     return (
-        <div>
+        <>
+            <div className={styles.words}>
+                {
+                    text.map((w, index) => <Word text={text} indexWord={index} typedText={typedText} word={w + " "}/>)
+                }
+            </div>
+            <input type={"text"} value={typedText} onChange={onChangeInput}/>
+            <div>
+                {typedText}
+            </div>
+        </>
+    )
+}
+
+const Word: React.FC<{ word: string, typedText: string, indexWord: number, text: Array<string>}> = ({text, word, typedText, indexWord}) => {
+    const wordArray: Array<string> = word.split("")
+    return (
+        <div className={styles.word}>
             {
-                text.map(w => <Word word={w + " "}/>)
+                wordArray.map((l, i) => <Letter text={text} indexWord={indexWord} indexLetter={i} typedText={typedText} letter={l}/>)
             }
         </div>
     )
 }
 
-const Word: React.FC<{word: string}> = ({word}) => {
-    const wordArray: Array<string> = word.split("")
-    return (
-        <>
-            {
-                wordArray.map(l => <Letter letter={l}/>)
-            }
-        </>
-    )
-}
+const Letter: React.FC<{ letter: string, typedText: string, indexWord: number, indexLetter: number, text: Array<string>}> = ({text, letter, typedText, indexWord, indexLetter}) => {
 
-const Letter: React.FC<{letter: string}> = ({letter}) => {
+    const typedTextArray = typedText.split(' ')
+
     return (
-        <>
+        <div className={styles.letter + ' ' + (indexWord + 1 >= typedTextArray.length ? '' : (typedTextArray[indexWord][indexLetter] === text[indexWord][indexLetter] ? styles.okey : styles.error))}>
             {letter}
-        </>
+        </div>
     )
 }
 
